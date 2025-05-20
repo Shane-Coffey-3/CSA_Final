ccccce2851152dc84d5ea9e4b7fd86345897adb5
Shane-Coffey-3 <shanejcoffey27@gmail.com>
2025-05-20 08:27:17 -0500

    Finalized firstFloor map

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         JFrame frame = new JFrame();
 
-        Game game = new Game(18, 2);
+        Game game = new Game(18, 1); // num on left is tile size
         frame.setSize(game.getWidthOfGame(), game.getHeightOfGame());
 
         frame.add(game);
